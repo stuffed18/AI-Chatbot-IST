ebb2ada613497646e3b4dc5ef446df01f8f2ec89
benson <benson>
2024-10-11 19:14:50 +1100

    made the first chat system with UI and stuff

--- aichatbot/Form1.Designer.vb
+++ aichatbot/Form1.Designer.vb
@@ -22,10 +22,72 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBoxInput = New System.Windows.Forms.TextBox()
+        Me.LabelTitle = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.White
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(28, 74)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(729, 371)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        Me.FlowLayoutPanel1.WrapContents = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Lime
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(567, 463)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 38)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "send"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBoxInput
+        '
+        Me.TextBoxInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxInput.Location = New System.Drawing.Point(28, 463)
+        Me.TextBoxInput.Multiline = True
+        Me.TextBoxInput.Name = "TextBoxInput"
+        Me.TextBoxInput.Size = New System.Drawing.Size(520, 71)
+        Me.TextBoxInput.TabIndex = 2
+        '
+        'LabelTitle
+        '
+        Me.LabelTitle.AutoSize = True
+        Me.LabelTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelTitle.Location = New System.Drawing.Point(21, 9)
+        Me.LabelTitle.Name = "LabelTitle"
+        Me.LabelTitle.Size = New System.Drawing.Size(263, 39)
+        Me.LabelTitle.TabIndex = 3
+        Me.LabelTitle.Text = "VB.ai chatbot 👍"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 546)
+        Me.Controls.Add(Me.LabelTitle)
+        Me.Controls.Add(Me.TextBoxInput)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBoxInput As TextBox
+    Friend WithEvents LabelTitle As Label
 End Class
--- aichatbot/Form1.vb
+++ aichatbot/Form1.vb
@@ -1,3 +1,51 @@
-﻿Public Class Form1
+﻿Imports System.Drawing.Imaging
 
+Public Class Form1
+    Dim Index As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SendMessage(True, TextBoxInput.Text)
+
+        Dim Answers = {"No.", "Sure buddy", "blud thought he was doing something with that", "Ok who asked", "Not my problem", "No one cares bro", "No thanks, I'm good.", "Maybe if you use 1% of your brain for once", "Beep boop im an AI chatbot ready at your service. Jsut kidding i dont care go die"}
+        Dim rnd = New Random()
+        SendMessage(False, Answers(rnd.Next(0, Answers.Count)))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'FlowLayoutPanel1.VerticalScroll.Visible = True
+        TextBoxInput.MaxLength = 200
+    End Sub
+
+    Private Sub SendMessage(UserSent As Boolean, Message As String)
+        Dim NewMessage As New Label
+        Index = Index + 1
+        NewMessage.Name = "chat" & Index
+
+        NewMessage.Text = Message
+        NewMessage.Font = New Font(NewMessage.Font.FontFamily.ToString(), 10)
+
+        If UserSent Then
+            NewMessage.BackColor = Color.Lime
+
+            Dim LeftMargin = FlowLayoutPanel1.Width - NewMessage.PreferredWidth - NewMessage.Padding.Right - 10
+            If LeftMargin > 0 Then
+                NewMessage.Margin = New Padding(LeftMargin, NewMessage.Padding.Top + 5, NewMessage.Padding.Right + 5, NewMessage.Padding.Bottom + 5)
+            Else
+                NewMessage.Margin = New Padding(FlowLayoutPanel1.Width - 300 - NewMessage.Padding.Right - 10, NewMessage.Padding.Top + 5, NewMessage.Padding.Right + 5, NewMessage.Padding.Bottom + 5)
+            End If
+
+            NewMessage.Anchor = AnchorStyles.Right
+            NewMessage.TextAlign = ContentAlignment.MiddleRight
+            NewMessage.Dock = DockStyle.Right
+        Else
+            NewMessage.BackColor = Color.Pink
+            NewMessage.Margin = New Padding(NewMessage.Padding.Left + 5, NewMessage.Padding.Top + 5, NewMessage.Padding.Right, NewMessage.Padding.Bottom + 5)
+        End If
+
+        NewMessage.AutoSize = True
+        NewMessage.MaximumSize = New Size(300, 0)
+
+        FlowLayoutPanel1.Controls.Add(NewMessage)
+        FlowLayoutPanel1.Controls.SetChildIndex(NewMessage, 0)
+        FlowLayoutPanel1.ScrollControlIntoView(NewMessage)
+    End Sub
 End Class
