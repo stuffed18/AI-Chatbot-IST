898fc3b5b8b34deb8af488121659884d4d9202a4
benson <benson>
2024-10-11 20:02:25 +1100

    Added functionality for chatbot to send images

--- aichatbot/Form1.vb
+++ aichatbot/Form1.vb
@@ -3,20 +3,36 @@
 Public Class Form1
     Dim Index As Integer = 0
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        SendMessage(True, TextBoxInput.Text)
+        If String.IsNullOrEmpty(TextBoxInput.Text) = False Then
+            SendMessage(True, TextBoxInput.Text)
+            TextBoxInput.Text = ""
 
-        Dim Answers = {"No.", "Sure buddy", "blud thought he was doing something with that", "Ok who asked", "Not my problem", "No one cares bro", "No thanks, I'm good.", "Maybe if you use 1% of your brain for once", "Beep boop im an AI chatbot ready at your service. Jsut kidding i dont care go die"}
-        Dim rnd = New Random()
-        SendMessage(False, Answers(rnd.Next(0, Answers.Count)))
+            Dim Answers = {"No.", "Sure buddy", "blud thought he was doing something with that", "Ok who asked", "Not my problem", "No one cares bro", "No thanks, I'm good.", "Maybe if you use 1% of your brain for once", "Beep boop im an AI chatbot ready at your service. Jsut kidding i dont care go die"}
+            Dim rnd = New Random()
+            SendMessage(False, Answers(rnd.Next(0, Answers.Count)))
+
+            If rnd.Next(10, 15) = 11 Then
+                SendImage()
+            End If
+        End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'FlowLayoutPanel1.VerticalScroll.Visible = True
         TextBoxInput.MaxLength = 200
 
         SendMessage(False, "Hi, I'm VBot! What can i do for you on this fine 23 degree day")
     End Sub
 
+    Private Sub SendImage()
+        Dim NewImage As New PictureBox
+        NewImage.Image = My.Resources.im_going_to_go_now__1_1
+        NewImage.SizeMode = PictureBoxSizeMode.StretchImage
+        NewImage.Size = New System.Drawing.Size(200, 200)
+
+        FlowLayoutPanel1.Controls.Add(NewImage)
+        FlowLayoutPanel1.Controls.SetChildIndex(NewImage, 0)
+        FlowLayoutPanel1.ScrollControlIntoView(NewImage)
+    End Sub
     Private Sub SendMessage(UserSent As Boolean, Message As String)
         Dim NewMessage As New Label
         Index = Index + 1
